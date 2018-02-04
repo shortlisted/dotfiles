fe41be477456f34222a4dad5034d4484b269f93a
Fredrik Lundqvist <fredrik.lundqvist@me.com>
2018-02-04 17:24:46 +0100

    vim plugin script and colorscheme

---
+++ vim/bundle/getgits.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+git clone git://github.com/rking/ag.vim 
+git clone git://github.com/flazz/vim-colorschemes.git
+git clone git://github.com/othree/eregex.vim
+git clone git://github.com/junegunn/goyo.vim
+git clone git://github.com/morhetz/gruvbox.git
+git clone git://github.com/wikitopian/hardmode.git
+git clone git://github.com/yggdroot/indentline
+git clone git://github.com/junegunn/limelight.vim
+git clone git://github.com/tmhedberg/matchit
+git clone git://github.com/scrooloose/nerdtree.git
+git clone git://github.com/vim-airline/vim-airline
+git clone git://github.com/vim-airline/vim-airline-themes
+git clone git://github.com/altercation/vim-colors-solarized.git
+git clone git://github.com/tpope/vim-dispatch.git
+git clone git://github.com/ajmwagar/vim-dues.git
+git clone git://github.com/tpope/vim-fugitive
+git clone git://github.com/plasticboy/vim-markdown
+git clone git://github.com/reedes/vim-pencil
+git clone git://github.com/tpope/vim-surround.git 
+git clone git://github.com/amix/vim-zenroom2
+
