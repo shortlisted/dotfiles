80d11c967890c8eb16efc4d202a45eda26928579
Fredrik Lundqvist <fredrik.lundqvist@me.com>
2018-06-08 16:41:50 +0200

    rewrote it completely

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -1,50 +1,120 @@
 #!/bin/bash
 ###########
-#Borrowed from https://github.com/michaeljsmalley
-###########
+## Script used to set up a directory with dotfiles
+## for easy version control through git
 
 # Variables
 ###########
+## folders and files
 
-DIR=~/dotfiles
-OLDDIR=~/dotfiles_old
-FILES="bash_profile gvimrc vim vimrc zshrc tmux tmux.conf" #list of files/folders
+dotdir=~/dotfiles
+olddot=~/dotfiles_old
+myfiles="bash_profile vimrc zshrc alias.zsh gitconfig gitignore_global"
+existfiles=""
 
-#oh-my-zsh
-##########
-## Check the status of oh-my-zsh
-## If installed oh-my-zsh is added to list of files to be changed
-## Will git clone oh-my-zsh if it's missing
-
-if [ -d ~/.oh-my-zsh ]; then
-	echo ".oh-my-zsh exists, proceeding" ;
-	FILES="$FILES oh-my-zsh";
+## create dotfiles dir if it doesn't exist
+if [ ! -d $dotdir ]; then 
+  mkdir -p $dotdir
 else
-	echo "no .oh-my-zsh, installing" ;
-	cd $DIR
-	git clone http://github.com/robbyrussell/oh-my-zsh.git
-	ln -s $DIR/oh-my-zsh ~/.oh-my-zsh;
+  printf "dotfile directory already exists @ $HOME/, aborting\n"
+  printf "delete the folder $HOME/$dotdir and rerun the script to use it.\n"
+  exit 0
 fi
 
-#Creating folders and symlinks
-##############################
-
-# create backup directory
-echo -n "Creating $OLDDIR for backup... "
-mkdir -p $OLDDIR
-echo "done"
-
-# change to dotfiles directory
-echo -n "Changing to the $DIR directory... "
-cd $DIR
-echo "done"
-
-# backup old files, create symlinks to dotfiles in $FILES
-for FILE in $FILES; do
-	echo "Moving existing dotfiles from $HOME to $OLDDIR"
-	mv ~/.$FILE $OLDDIR/
-	echo "Creating symlinks in home directory."
-	ln -s $DIR/$FILE ~/.$FILE
+## look for exisiting dotfiles in ~/
+printf "checking if any dotfile already exists...\n"
+for file in $myfiles; do
+  if [ -f .$file ]; then
+    printf "dotfile .$file found...\n";
+    existfiles="$existfiles $file"
+  else
+    printf "no .$file found...\n"
+  fi
 done
 
-echo '\nSymlinks complete'
+## if existing dotfiles found the variable will not be empty:
+[ -n "$existfiles" ] && printf "Old dotfiles found: $existfiles\nWill be stored in $olddot\n"
+
+# oh-my-zsh
+###########
+## Check status of oh-my-zsh
+## Downloads if needed
+
+if [ -d .oh-my-zsh ]; then
+  printf ".oh-my-zsh exists, proceeding\n"
+  myfiles="$myfiles oh-my-zsh"
+elif [ -L .oh-my-zsh ]; then
+  printf "oh-my-zsh as symlink exists...\n"
+else
+  printf "no .oh-my-zsh, cloning...\n\n"
+  git clone https://github.com/robbyrussell/oh-my-zsh.git dotfiles/oh-my-zsh
+  printf "\n"
+  ln -s $dotdir/oh-my-zsh .oh-my-zsh
+fi
+
+# Folders and symlinks
+######################
+## all already existing dotfiles are moved to _old directory
+## creates symlinks from home directory to dotfiles directory
+## ~/.filename will link to ~/dotfiles/filename
+if [ -n "$existfiles" ]; then 
+  mkdir $olddot
+  for file in $existfiles; do
+    echo "Moving $file from $HOME to $olddot"
+    mv ./.$file $olddot/$file
+  done
+fi
+
+for file in $myfiles; do
+  echo "Creating symlinks in $HOME/.$file to $dotdir"
+  ln -s $dotdir/$file .$file
+  #[ -L .$file ]
+done
+
+printf "Symlinks complete!\n\n"
+
+# git
+#####
+## using a local config file with 
+## user data to keep name/address
+## out of the git repo
+
+read -p "Create a gitconfig_local file (y/n)?  " -n 1 -r
+echo    # (optional) move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+
+if [ ! -f dotfiles/gitconfig_local ]; then
+printf "\n************************\n"
+printf "*create gitconfig_local*\n"
+printf "************************\n"
+printf "\nThe following information
+will be signed to all git commits
+made from this computer\n\n"
+printf "First name: "
+read first
+printf "Last name: "
+read last
+printf "Email address: "
+read email
+
+cat > dotfiles/gitconfig_local << EOF
+[user]
+       name = $first $last
+       email = $email
+EOF
+fi
+printf "\ngitconfig_local created in $dotdir\n"
+printf "Contents:\n"
+cat dotfiles/gitconfig_local
+
+fi
+printf "\nDotfiles folder and symlinks created.\n
+Use source ~/dotfiles/zshrc to load.\n\n"
+
+if [ -d $olddot ]; then
+  printf "Contents of $olddot\n"
+  ls -a $olddot
+fi
+printf "Contents of $dotdir\n"
+ls -a $dotdir
